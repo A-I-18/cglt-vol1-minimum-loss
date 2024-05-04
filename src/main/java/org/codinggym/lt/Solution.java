622d8ab0c6a0c356eb70de2ad463461d023c2ea0
A-I-18 <antonio.ionta.18@gmail.com>
2024-05-04 15:53:31 +0200

    feat: logic implementation

--- src/main/java/org/codinggym/lt/Solution.java
+++ src/main/java/org/codinggym/lt/Solution.java
@@ -1,6 +1,7 @@
 package org.codinggym.lt;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -27,10 +28,25 @@ public class Solution {
   }
 
   public static int computeMinimumLoss(int n, List<Integer> values) {
-    //TODO
-    // Compute all the possible differences between a value in the list and all the successive elements (function)
-    // Extract the higher negative value (discard the positive ones) (function)
-    // Return
-    return 0;
+    // Compute all the possible differences between a value in the list and all the successive elements (discard the negative ones) (function)
+    List<Integer> losses = computeLosses(values);
+    // Extract the lower value (function)
+    return Collections.min(losses);
   }
+
+  private static List<Integer> computeLosses(List<Integer> values) {
+    List<Integer> list = new ArrayList<>();
+
+    for(int i = 0; i < values.size(); i++) {
+      for(int j = i+1; j < values.size(); j++) {
+        int loss = values.get(i)- values.get(j);
+        if(loss >= 0) {
+          list.add(loss);
+        }
+      }
+    }
+
+    return list;
+  }
+
 }
